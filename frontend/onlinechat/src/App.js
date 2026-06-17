0df4d295923fb2d61ca2317c6a7fee2e7f9274c3
Vladimir Belskiy <belskiy548@gmail.com>
2026-06-17 19:07:44 +0300

    Finished chat logic and page design

--- frontend/onlinechat/src/App.js
+++ frontend/onlinechat/src/App.js
@@ -1,9 +1,42 @@
+import { useState } from "react";
 import { WaitingRoom } from "./components/WaitingRoom";
+import {HubConnectionBuilder} from "@microsoft/signalr"
+import { Chat } from "./components/Chat";
 
 function App() {
+  const [connection, setConnection] = useState(null); 
+  const [chatRoom, setChatRoom] = useState(""); 
+  const [messages, setMessages] = useState([]); 
+
+  const joinChat = async (userName, chatRoom) => {
+    var connection = new HubConnectionBuilder()
+        .withUrl("http://localhost:5124/chat")
+        .withAutomaticReconnect()
+        .build();
+
+        connection.on("ReceiveMessage", (userName, message) => {
+          setMessages((messages) => [...messages, {userName, message}]) 
+        })
+        try{
+          await connection.start(); 
+          await connection.invoke("JoinChat", {userName, chatRoom})
+          setConnection(connection); 
+          setChatRoom(chatRoom); 
+        }
+        catch(error){
+          console.log(error); 
+        }
+  }
+  const sendMessage = (message) => {
+    connection.invoke("SendMessage", message); 
+  }
+  const closeChat = async () => {
+    await connection.stop(); 
+    setConnection(null); 
+  }
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <WaitingRoom />
+      {connection ? <Chat messages={messages} chatRoom={chatRoom} sendMessage={sendMessage} closeChat={closeChat}/> : <WaitingRoom joinChat={joinChat}/>}
     </div>
   );
 }
--- frontend/onlinechat/src/components/WaitingRoom.jsx
+++ frontend/onlinechat/src/components/WaitingRoom.jsx
@@ -1,16 +1,24 @@
 import { Button, Heading, Input, Text } from "@chakra-ui/react"
+import { useState } from "react";
 
-export const WaitingRoom = () => {
+export const WaitingRoom = ({joinChat}) => {
+    const [userName, setUserName] = useState(); 
+    const [chatRoom, setChatRoom] = useState(); 
+
+    const onSubmit = (e) => {
+        e.preventDefault(); 
+        joinChat(userName, chatRoom); 
+    }
     return (
-        <form className="max-w-sm w-full bg-white p-8 rounded shadow-lg">
+        <form onSubmit={onSubmit} className="max-w-sm w-full bg-white p-10 rounded-lg shadow-lg">
             <Heading>Онлайн чат</Heading>
             <div className="mb-4">
                 <Text fontSize={"sm"}>Имя пользователя</Text>
-                <Input name="userName" placeholder="Введите ваше имя"/>
+                <Input onChange={(e) => setUserName(e.target.value)} name="userName" placeholder="Введите ваше имя"/>
             </div>
             <div className="mb-4">
                 <Text fontSize={"sm"}>Название чата</Text>
-                <Input name="chatRoom" placeholder="Введите название чата"/>
+                <Input onChange={(e) => setChatRoom(e.target.value)} name="chatRoom" placeholder="Введите название чата"/>
             </div>
             <Button type="submit" colorScheme="blue">
                 Присоединиться
